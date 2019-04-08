65a12e68e66c952c1c6ee5ed466c247f8def85f2
costawilliam <costawilliam@outlook.com>
2019-04-08 13:59:38 -0300

    DepartmentForm (dialog) design

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,12 @@
+package gui.util;
+
+import java.awt.event.ActionEvent;
+
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	public static Stage currentStage(javafx.event.ActionEvent event) {		
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();		
+	}
+}
